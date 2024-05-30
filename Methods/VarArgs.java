b2887c14f6ebdafef2acec59a39d5c906de7c403
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-05-30 16:10:52 +0530

    Update VarArgs.java

--- Methods/VarArgs.java
+++ Methods/VarArgs.java
@@ -6,7 +6,7 @@ public class VarArgs {
     public static void main(String[] args) {
         va(1,4,6,23,4,5);
     }
-    static void va(int ...a){   //can have multiple parameter inputs which will be stored in an array.
+    static void va(int ...a){   //can have multiple parameter inputs which will be stored in an Array.
         System.out.println("First element: "+a[0]);
         System.out.println(Arrays.toString(a));
     }
