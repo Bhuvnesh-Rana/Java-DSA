43da4ffdb0859ce353c8c724044a929759f020c2
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-06-07 05:35:47 +0000

    Patterns.

---
+++ Patterns/DigitTriangle.java
@@ -0,0 +1,18 @@
+package Patterns;
+
+public class DigitTriangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for(int j = i; j <= 4 ; j++){
+                System.out.print("  ");
+            }
+            for(int j = i; j >= 1; j--){
+                System.out.print(j+" ");
+            }
+            for(int j = 2; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
