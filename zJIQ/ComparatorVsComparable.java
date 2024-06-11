6f02df0bd96e1fe3b068769ac8b429981b733723
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-06-11 07:55:53 +0000

    Comparator vs comparable

---
+++ zJIQ/ComparatorVsComparable.java
@@ -0,0 +1,35 @@
+package zJIQ;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class ComparatorVsComparable {
+//while sorting List of integer can be sorted but list of obj cannot(on what basis/field u want to sort.)
+//comparator used for multiple sorting logics and comparable used for single logic.
+    public static void main(String[] args) {
+        Comparator<EmployeeComparableVsComparator> com = new Comparator<EmployeeComparableVsComparator>() {
+
+            @Override
+            public int compare(EmployeeComparableVsComparator o1, EmployeeComparableVsComparator o2) {
+                if (o1.getId()>o2.getId()) {
+                    return 1;
+                }
+                return -1;
+            }
+            
+        };
+
+        ArrayList<EmployeeComparableVsComparator> obj = new ArrayList<>();
+        obj.add(new EmployeeComparableVsComparator(1, "Abcd", "IT", "Chd"));
+        obj.add(new EmployeeComparableVsComparator(4, "Efgh", "HR", "Moh"));
+        obj.add(new EmployeeComparableVsComparator(3, "Ijkl", "QA", "Mum"));
+        obj.add(new EmployeeComparableVsComparator(2, "Mnop", "IT", "Noi"));
+
+        Collections.sort(obj, com);
+        for (EmployeeComparableVsComparator employeeComparableVsComparator : obj) {
+            System.out.println(employeeComparableVsComparator);
+        }
+
+    }
+}
