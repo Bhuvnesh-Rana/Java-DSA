9a2ecaabe0eccc4c87a97c86d3e2bd705d6ccfb0
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-06-25 09:54:28 +0000

    No. of digits of a no. sum.

---
+++ ZJIQ/NumberOfDigitsSum.java
@@ -0,0 +1,15 @@
+package ZJIQ;
+
+public class NumberOfDigitsSum {
+    public static void main(String[] args) {
+        int num = 12345;
+        int sum = 0;
+
+        while (num != 0) {
+            int a = num % 10;
+            sum = sum + a;
+            num = num / 10;
+        }
+        System.out.println("Sum of digits of numbers is: "+sum);
+    }
+}
