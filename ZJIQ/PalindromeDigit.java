1c922b155b231188d1977e47a08cf72efc7d4fce
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-06-25 06:12:04 +0000

    No. palindrome.

--- ZJIQ/PalindromeDigit.java
+++ ZJIQ/PalindromeDigit.java
@@ -3,5 +3,18 @@ package ZJIQ;
 public class PalindromeDigit {
     public static void main(String[] args) {
         int a = 1224221;
+        int a1 = a;
+        int b = 0;
+
+        while (a != 0) {
+            b = (b * 10) + a % 10;
+            a = a / 10;
+        }
+        if (a1 == b) {
+            System.out.println("No. is a palindrome");
+        } else {
+            System.out.println("No. is not a palindrome.");
+        }
+        
     }
 }
