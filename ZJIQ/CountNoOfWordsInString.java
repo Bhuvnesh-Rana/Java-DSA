ceea6eecd8eb1775ca5ec73a83a94db1e1c18042
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-06-27 10:31:47 +0000

    Count no. of words in String with try catch.

--- ZJIQ/CountNoOfWordsInString.java
+++ ZJIQ/CountNoOfWordsInString.java
@@ -1,18 +1,34 @@
 package ZJIQ;
 
+class MyException extends Exception{
+    MyException(String s){
+        super(s);
+    }
+}
+
 public class CountNoOfWordsInString {
     public static void main(String[] args) {
-        String a = "Happy go lucky go";
+        String a = "Happy go  lucky go";
+        boolean flag = true;
         int count = a.length();
+
         for(int i = 0; i <= a.length()-1; i++){
             if(a.charAt(i) == ' ' && a.charAt(i+1) != ' '){
                 count--;
             }
+            
             else if (a.charAt(i) == ' ' && a.charAt(i+1) == ' ') {
-                System.out.println("Invalid<---> two spaces togather at index: "+i);
-                break; 
+                flag = false;
+                try {
+                    throw new MyException("Invalid String entered.");
+                } catch (MyException e) {
+                    // System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+                break;
             }
         }
-        System.out.println(count);
+        if(flag)
+            System.out.println(count);
     }
 }
